550eab5ed5ebc3ee04f57e08c1943e687f322656
Zebedee Pinto <service@gruntwrk.com>
2026-01-17 18:48:46 +0200

    Landing: lighter headline and tighter spacing under logo

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,11 @@ export default function Home() {
       <div className="container">
         <div className="centerWrap">
           <section className="centerContent">
-            {/* REAL LOGO (SVG preferred) */}
-            {/* If you upload PNG instead, change src to "/brand/gruntwrk-g.png" */}
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img className="spinLogo" src="/brand/gruntwrk-g.svg" alt="GruntWrk" />
 
             <h1 className="h1">Get work done. Find work fast.</h1>
+
             <p className="sub">Post jobs. Offer your skills. Connect with people.</p>
 
             <div className="ctaRow">
